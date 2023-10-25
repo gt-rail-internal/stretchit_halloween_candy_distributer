560b1cae24882c7f8328f96f4c2934a378c55feb
Abivishaq <b.abivishaq@gmail.com>
2023-10-25 19:41:16 -0400

    camera_random_movement

--- src/candy_distributor/nodes/random_cam_move_test.py
+++ src/candy_distributor/nodes/random_cam_move_test.py
@@ -8,6 +8,7 @@ from trajectory_msgs.msg import JointTrajectoryPoint
 import hello_helpers.hello_misc as hm
 # import tf
 import tf2_ros
+import random
 
 from candy_distributor.srv import candy_pick_srv, candy_pick_srvResponse
 
@@ -39,7 +40,8 @@ class ArucoGraspCandy(hm.HelloNode):
   def candy_pick_service_callback(self,req):
     rospy.loginfo("received request")
     if(req.candy_no in [1,2,3]):
-      self.aruco_grasp_candy_command(req.candy_no)
+      # self.aruco_grasp_candy_command(req.candy_no)
+      self.camera_scan_non_blocking()
       return candy_pick_srvResponse(True)
     else:
       return candy_pick_srvResponse(False)
@@ -118,15 +120,62 @@ class ArucoGraspCandy(hm.HelloNode):
     return max(min(height,1.0),0.3)
   def wrap_extension(self,extension):
     return max(min(extension,0.5),0.0)
+  def wrap_pan(self,pan):
+    return max(min(pan,1.5),-4.0)
+  def wrap_tilt(self,tilt):
+    return max(min(tilt,0.4),-1.0)
   def camera_scan_non_blocking(self):
-    camera_left_pose = [self.head_pan+0.5, self.head_tilt]
-    camera_right_pose = [self.head_pan-0.5, self.head_tilt]
+    # creating an array of pan values from -1.5 to 1.5 with random increments in between 0.1 to 1.5
+    scan_min = -1.0
+    scan_max = 1
+    head_tilt = -0.2
+    head_pan = -1.5
+    pan_rand_weight = 0.2
+    pan_rand_const = 0.1
+    tilt_rand_weight = 2.0
+    pan_rand_buffer_size = 10
+    tilt_rand_buffer_size = 30
+    head_pan_array = [head_pan+scan_min]
+    random_queue = []
+    for i in range(pan_rand_buffer_size):
+      random_queue.append(random.random())
+    for i in range(100):
+      rand_no = random.random()
+      random_queue.pop()
+      random_queue.append(rand_no)
+      rand = sum(random_queue)/len(random_queue)
+      rand=rand*pan_rand_weight+pan_rand_const
+      head_pan_array.append(head_pan_array[-1]+rand)
+      if(head_pan_array[-1]>head_pan+scan_max):
+        break
+    for i in range(100):
+      rand_no = random.random()
+      random_queue.pop()
+      random_queue.append(rand_no)
+      rand = sum(random_queue)/len(random_queue)
+      rand=rand*pan_rand_weight+pan_rand_const
+      head_pan_array.append(head_pan_array[-1]-rand)
+      if(head_pan_array[-1]<head_pan+scan_min):
+        break
     trajectory_goal = FollowJointTrajectoryGoal()
-    trajectory_goal.trajectory.joint_names = self.camera_joint_names
+    trajectory_goal.trajectory.joint_names = ['joint_head_pan','joint_head_tilt']
     trajectory_goal.trajectory.header.stamp = rospy.Time(0.0)
     trajectory_goal.trajectory.header.frame_id = 'base_link'
-    trajectory_goal.trajectory.points.append(self.create_trajectory_point(camera_left_pose))
-    trajectory_goal.trajectory.points.append(self.create_trajectory_point(camera_right_pose))
+    random_queue = []
+    for i in range(tilt_rand_buffer_size):
+      random_queue.append(random.random()-0.5)
+    print('head_array_length:',len(head_pan_array))
+    for pan in head_pan_array:
+      random_no = random.random()-0.5
+      random_queue.pop()
+      random_queue.append(random_no)
+      random_tilt = sum(random_queue)/len(random_queue)
+      random_tilt = random_tilt*tilt_rand_weight
+      trajectory_goal.trajectory.points.append(self.create_trajectory_point([self.wrap_pan(pan),self.wrap_tilt(head_tilt-random_tilt)]))
+    trajectory_goal.trajectory.points.append(self.create_trajectory_point([self.wrap_pan(head_pan),self.wrap_tilt(head_tilt)]))
+    
+    # trajectory_goal.trajectory.points.append(self.create_trajectory_point(camera_left_pose))
+    # trajectory_goal.trajectory.points.append(self.create_trajectory_point(camera_right_pose))
     self.trajectory_client.send_goal(trajectory_goal)
     return trajectory_goal
   def scan_and_get_aruco(self,candy_frame_id):
